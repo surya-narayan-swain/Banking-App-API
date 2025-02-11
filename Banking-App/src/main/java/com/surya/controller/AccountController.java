99625c0c09a3d8f3a03d8f2884bb2f02fac793d3
surya-narayan-swain <surya.swain@gmail.com>
2025-02-12 03:11:12 +0530

    Deposit+Withdraw+Get_All_Accounts+Delete_Accounts -Rest_API

--- Banking-App/src/main/java/com/surya/controller/AccountController.java
+++ Banking-App/src/main/java/com/surya/controller/AccountController.java
@@ -1,11 +1,16 @@
 package com.surya.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +38,42 @@ public class AccountController {
 	{
 		AccountDto accountDto=accountService.getAccountById(id);
 		return new ResponseEntity<>(accountDto,HttpStatus.OK);
+	}
+	
+	//Deposit Rest Api
+	@PutMapping("/{id}/deposit")
+	public ResponseEntity<AccountDto> deposit(@PathVariable long id,@RequestBody Map<String,Double> request)
+	{
+		Double amount=request.get("amount");
+		AccountDto accountDto=accountService.deposit(id,amount);
+		return new ResponseEntity<>(accountDto,HttpStatus.OK);
+	}
+	
+	//Withdraw RestApi
+	@PutMapping("/{id}/withdraw")
+	public ResponseEntity<AccountDto> withdraw(@PathVariable long id,@RequestBody Map<String, Double> request)
+	{
+		Double ammount=request.get("amount");
+		AccountDto accountDto=accountService.withdraw(id, ammount);
+		return new ResponseEntity<>(accountDto,HttpStatus.OK);
+	}
+	
+	//Get All Accounts RestApi
+	@GetMapping
+	public ResponseEntity<List<AccountDto>> getAllAccounts()
+	{
+		List<AccountDto> accounts=accountService.getAllAccounts();
+		return new ResponseEntity<>(accounts,HttpStatus.OK);
+
+	}
+	
+	//Delete Account Rest Api
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteAccount(@PathVariable long id)
+	{
+		accountService.deleteAccount(id);
+		return new ResponseEntity<String>("Account is deleted successfully !",HttpStatus.OK);
+		
 	}
 	
 }
--- Banking-App/src/main/java/com/surya/service/AccountService.java
+++ Banking-App/src/main/java/com/surya/service/AccountService.java
@@ -1,9 +1,15 @@
 package com.surya.service;
 
+import java.util.List;
+
 import com.surya.dto.AccountDto;
 
 public interface AccountService {
 
 	AccountDto createAccount(AccountDto accountdto);
 	AccountDto getAccountById(long id);
+	AccountDto deposit(long id,double amount);
+	AccountDto withdraw(long id,double amount);
+	List<AccountDto> getAllAccounts();
+	void deleteAccount(long id);
 }
